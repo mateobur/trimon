75e1d3cfcc855785a3c93927728ed969cc320185
Mateo Burillo <mburillo@zentyal.com>
2015-07-11 01:19:50 +0200

    pep8

--- trimon.py
+++ trimon.py
@@ -19,10 +19,16 @@ import imaplib
 import email
 import ConfigParser
 
-def idGenerator(size=10, chars=string.ascii_uppercase + string.digits + string.ascii_lowercase):
+
+def idGenerator(
+        size=10,
+        chars=string.ascii_uppercase +
+        string.digits +
+        string.ascii_lowercase):
     return ''.join(random.choice(chars) for _ in range(size))
 
-def sendAlert(module, body, alertType = 'nonPassed'):
+
+def sendAlert(module, body, alertType='nonPassed'):
     if not config.getboolean('Alerting', 'enabled'):
         return
     recipient = config.get('Alerting', 'recipient')
@@ -41,7 +47,11 @@ def sendAlert(module, body, alertType = 'nonPassed'):
     msg['To'] = recipient
     msg['Date'] = day + ', ' + date + ' -0000'
     server = config.get('Alerting', 'sender_server')
-    session = smtplib.SMTP(server, config.getint('Alerting','server_SMTP_port'))
+    session = smtplib.SMTP(
+        server,
+        config.getint(
+            'Alerting',
+            'server_SMTP_port'))
     session.ehlo()
     session.starttls()
     session.ehlo()
@@ -49,19 +59,24 @@ def sendAlert(module, body, alertType = 'nonPassed'):
     session.sendmail(sender, recipient, msg.as_string())
     session.quit()
 
+
 def checkPing():
-    return subprocess.check_call(['ping', '-c 4', config.get('General', 'target')], stdout=open(os.devnull, 'w')) == 0, "no ping"
+    return subprocess.check_call(
+        ['ping', '-c 4', config.get('General', 'target')],
+        stdout=open(os.devnull, 'w')) == 0, "no ping"
+
 
 def checkPorts():
     portlist = json.loads(config.get('Ports', 'ports_list'))
     for port in portlist:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(3)
-        result = sock.connect_ex((config.get('General', 'target'),port))
+        result = sock.connect_ex((config.get('General', 'target'), port))
         sock.close()
         if result != 0:
             return False, "port " + str(port) + " is closed"
     return True, None
+
 
 def checkWebs():
     browser = mechanize.Browser()
@@ -69,7 +84,10 @@ def checkWebs():
     browser.set_cookiejar(cj)
     browser.set_handle_redirect(True)
     browser.set_handle_robots(False)
-    browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+    browser.addheaders = [
+        ('User-agent',
+         'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) ' +
+         'Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
     urls = json.loads(config.get('Webs', 'url_check'))
     for url_check in urls:
         response = browser.open(url_check[0])
@@ -78,35 +96,65 @@ def checkWebs():
         text = response.read()
         for i in range(1, len(url_check)):
             if str(url_check[i]) not in text:
-                return False, "String '" + url_check[i] + "' not found in webpage '" + url_check[0] + "'"
+                return False, "String '" + \
+                    url_check[i] + "' not found in webpage '" + \
+                    url_check[0] + "'"
     return True, None
 
+
 def checkLoad():
-    output = subprocess.check_output("ssh " + config.get('General', 'ssh_login') + " cat /proc/loadavg", shell=True)
+    output = subprocess.check_output(
+        "ssh " +
+        config.get(
+            'General',
+            'ssh_login') +
+        " cat /proc/loadavg",
+        shell=True)
     if float(output.split()[0]) > config.getfloat("Load", "threshold"):
         return False, "System load above threshold: " + output
     return True, None
 
+
 def checkServices():
     service_list = json.loads(config.get('Services', 'services_list'))
     for service in service_list:
-        output = subprocess.check_output("ssh " + config.get('General', 'ssh_login') +" /etc/init.d/" + service + " status", shell=True)
-        if not 'running' in output:
+        output = subprocess.check_output(
+            "ssh " +
+            config.get(
+                'General',
+                'ssh_login') +
+            " /etc/init.d/" +
+            service +
+            " status",
+            shell=True)
+        if 'running' not in output:
             return False
     return True
 
+
 def checkMem():
-    output = subprocess.check_output("ssh " + config.get('General', 'ssh_login') + " free", shell=True).split()
+    output = subprocess.check_output(
+        "ssh " +
+        config.get(
+            'General',
+            'ssh_login') +
+        " free",
+        shell=True).split()
     swapTotal = output[output.index('Swap:') + 1]
     swapUsed = output[output.index('Swap:') + 2]
-    if float(swapUsed) / float(swapTotal) > config.getfloat("Mem", "swap_used_threshold"):
-        return False
+    if float(swapUsed) / float(swapTotal) > \
+       config.getfloat("Mem", "swap_used_threshold"):
+            return False
     return True, None
+
 
 def checkMail():
     messageID = idGenerator()
     msg = MIMEText('<code>' + messageID + '</code>')
-    session = smtplib.SMTP(config.get('Mail', 'sender_server'), config.getint('Mail', 'sender_SMTP_port'))
+    session = smtplib.SMTP(
+        config.get(
+            'Mail', 'sender_server'), config.getint(
+            'Mail', 'sender_SMTP_port'))
     now = datetime.now(pytz.utc)
     day = now.strftime('%a')
     date = now.strftime('%d %b %Y %X')
@@ -117,18 +165,27 @@ def checkMail():
     session.ehlo()
     session.starttls()
     session.ehlo()
-    session.login(config.get('Mail', 'sender_addr'), config.get('Mail', 'sender_pass'))
-    session.sendmail(config.get('Mail', 'sender_addr'), config.get('Mail', 'recipient_addr'), msg.as_string())
+    session.login(
+        config.get(
+            'Mail', 'sender_addr'), config.get(
+            'Mail', 'sender_pass'))
+    session.sendmail(
+        config.get(
+            'Mail', 'sender_addr'), config.get(
+            'Mail', 'recipient_addr'), msg.as_string())
     session.quit()
-    time.sleep(2)
+    time.sleep(10)
     M = imaplib.IMAP4_SSL(config.get('Mail', 'recipient_server'))
-    M.login(config.get('Mail', 'recipient_addr'), config.get('Mail', 'recipient_pass'))
+    M.login(
+        config.get(
+            'Mail', 'recipient_addr'), config.get(
+            'Mail', 'recipient_pass'))
     M.select('INBOX')
     status, data = M.sort('REVERSE DATE', 'UTF-8', 'ALL')
     email_id = data[0].split()[0]
     status, data = M.fetch(email_id, '(RFC822)')
     msg = email.message_from_string(data[0][1])
-    code = re.search('<code>(.*)</code>',msg.get_payload()).group(1)
+    code = re.search('<code>(.*)</code>', msg.get_payload()).group(1)
     if code == messageID:
         if (config.getboolean('Mail', 'delete_test_email')):
             M.store(email_id, '+FLAGS', '\\Deleted')
@@ -153,7 +210,7 @@ for check in checks:
             if not success:
                 all_success = False
                 sendAlert(check, reason)
-        except Exception, e:
+        except Exception as e:
             all_success = False
             sendAlert(check, str(e), 'exception')
 
