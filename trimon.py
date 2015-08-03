bca94631177422065adbf9a6c296376a048dccf9
Mateo Burillo <mburillo@zentyal.com>
2015-08-03 20:31:05 +0200

    Minor clean up

--- trimon.py
+++ trimon.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
 import smtplib
-import sys
 import string
 import random
 from datetime import datetime
@@ -26,57 +25,57 @@ def idGenerator(
         chars=string.ascii_uppercase +
         string.digits +
         string.ascii_lowercase):
-    return ''.join(random.choice(chars) for _ in range(size))
+    return "".join(random.choice(chars) for _ in range(size))
 
 
 def handler(signum, frame):
     raise Exception("Time out exception")
 
 
-def sendAlert(module, body, alertType='nonPassed'):
-    if not config.getboolean('Alerting', 'enabled'):
+def sendAlert(module, body, alertType="nonPassed"):
+    if not config.getboolean("Alerting", "enabled"):
         return
-    recipient = config.get('Alerting', 'recipient')
-    sender = config.get('Alerting', 'sender_addr')
+    recipient = config.get("Alerting", "recipient")
+    sender = config.get("Alerting", "sender_addr")
     msg = MIMEText(body)
     now = datetime.now(pytz.utc)
-    day = now.strftime('%a')
-    date = now.strftime('%d %b %Y %X')
-    if alertType == 'exception':
-        msg['Subject'] = 'Exception running module: ' + module
-    if alertType == 'nonPassed':
-        msg['Subject'] = 'Alert in module: ' + module
-    if alertType == 'success':
-        msg['Subject'] = 'Successful run'
-    msg['From'] = sender
-    msg['To'] = recipient
-    msg['Date'] = day + ', ' + date + ' -0000'
-    server = config.get('Alerting', 'sender_server')
+    day = now.strftime("%a")
+    date = now.strftime("%d %b %Y %X")
+    if alertType == "exception":
+        msg["Subject"] = "Exception running module: " + module
+    if alertType == "nonPassed":
+        msg["Subject"] = "Alert in module: " + module
+    if alertType == "success":
+        msg["Subject"] = "Successful run"
+    msg["From"] = sender
+    msg["To"] = recipient
+    msg["Date"] = day + ", " + date + " -0000"
+    server = config.get("Alerting", "sender_server")
     session = smtplib.SMTP(
         server,
         config.getint(
-            'Alerting',
-            'server_SMTP_port'))
+            "Alerting",
+            "server_SMTP_port"))
     session.ehlo()
     session.starttls()
     session.ehlo()
-    session.login(sender, config.get('Alerting', 'sender_pass'))
+    session.login(sender, config.get("Alerting", "sender_pass"))
     session.sendmail(sender, recipient, msg.as_string())
     session.quit()
 
 
 def checkPing():
     return subprocess.check_call(
-        ['ping', '-c 4', config.get('General', 'target')],
-        stdout=open(os.devnull, 'w')) == 0, "no ping"
+        ["ping", "-c 4", config.get("General", "target")],
+        stdout=open(os.devnull, "w")) == 0, "no ping"
 
 
 def checkPorts():
-    portlist = json.loads(config.get('Ports', 'ports_list'))
+    portlist = json.loads(config.get("Ports", "ports_list"))
     for port in portlist:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(3)
-        result = sock.connect_ex((config.get('General', 'target'), port))
+        result = sock.connect_ex((config.get("General", "target"), port))
         sock.close()
         if result != 0:
             return False, "port " + str(port) + " is closed"
@@ -90,10 +89,10 @@ def checkWebs():
     browser.set_handle_redirect(True)
     browser.set_handle_robots(False)
     browser.addheaders = [
-        ('User-agent',
-         'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) ' +
-         'Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-    urls = json.loads(config.get('Webs', 'url_check'))
+        ("User-agent",
+         "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) " +
+         "Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1")]
+    urls = json.loads(config.get("Webs", "url_check"))
     for url_check in urls:
         response = browser.open(url_check[0])
         if response.code != 200:
@@ -111,8 +110,8 @@ def checkLoad():
     output = subprocess.check_output(
         "ssh " +
         config.get(
-            'General',
-            'ssh_login') +
+            "General",
+            "ssh_login") +
         " cat /proc/loadavg",
         shell=True)
     if float(output.split()[0]) > config.getfloat("Load", "threshold"):
@@ -121,18 +120,18 @@ def checkLoad():
 
 
 def checkServices():
-    service_list = json.loads(config.get('Services', 'services_list'))
+    service_list = json.loads(config.get("Services", "services_list"))
     for service in service_list:
         output = subprocess.check_output(
             "ssh " +
             config.get(
-                'General',
-                'ssh_login') +
+                "General",
+                "ssh_login") +
             " /etc/init.d/" +
             service +
             " status",
             shell=True)
-        if 'running' not in output:
+        if "running" not in output:
             return False
     return True
 
@@ -141,12 +140,12 @@ def checkMem():
     output = subprocess.check_output(
         "ssh " +
         config.get(
-            'General',
-            'ssh_login') +
+            "General",
+            "ssh_login") +
         " free",
         shell=True).split()
-    swapTotal = output[output.index('Swap:') + 1]
-    swapUsed = output[output.index('Swap:') + 2]
+    swapTotal = output[output.index("Swap:") + 1]
+    swapUsed = output[output.index("Swap:") + 2]
     if float(swapUsed) / float(swapTotal) > \
        config.getfloat("Mem", "swap_used_threshold"):
             return False
@@ -155,45 +154,45 @@ def checkMem():
 
 def checkMail():
     messageID = idGenerator()
-    msg = MIMEText('<code>' + messageID + '</code>')
+    msg = MIMEText("<code>" + messageID + "</code>")
     session = smtplib.SMTP(
         config.get(
-            'Mail', 'sender_server'), config.getint(
-            'Mail', 'sender_SMTP_port'))
+            "Mail", "sender_server"), config.getint(
+            "Mail", "sender_SMTP_port"))
     now = datetime.now(pytz.utc)
-    day = now.strftime('%a')
-    date = now.strftime('%d %b %Y %X')
-    msg['Subject'] = 'Trimon check mail'
-    msg['From'] = config.get('Mail', 'sender_addr')
-    msg['To'] = config.get('Mail', 'recipient_addr')
-    msg['Date'] = day + ', ' + date + ' -0000'
+    day = now.strftime("%a")
+    date = now.strftime("%d %b %Y %X")
+    msg["Subject"] = "Trimon check mail"
+    msg["From"] = config.get("Mail", "sender_addr")
+    msg["To"] = config.get("Mail", "recipient_addr")
+    msg["Date"] = day + ", " + date + " -0000"
     session.ehlo()
     session.starttls()
     session.ehlo()
     session.login(
         config.get(
-            'Mail', 'sender_addr'), config.get(
-            'Mail', 'sender_pass'))
+            "Mail", "sender_addr"), config.get(
+            "Mail", "sender_pass"))
     session.sendmail(
         config.get(
-            'Mail', 'sender_addr'), config.get(
-            'Mail', 'recipient_addr'), msg.as_string())
+            "Mail", "sender_addr"), config.get(
+            "Mail", "recipient_addr"), msg.as_string())
     session.quit()
     time.sleep(10)
-    M = imaplib.IMAP4_SSL(config.get('Mail', 'recipient_server'))
+    M = imaplib.IMAP4_SSL(config.get("Mail", "recipient_server"))
     M.login(
         config.get(
-            'Mail', 'recipient_addr'), config.get(
-            'Mail', 'recipient_pass'))
-    M.select('INBOX')
-    status, data = M.sort('REVERSE DATE', 'UTF-8', 'ALL')
+            "Mail", "recipient_addr"), config.get(
+            "Mail", "recipient_pass"))
+    M.select("INBOX")
+    status, data = M.sort("REVERSE DATE", "UTF-8", "ALL")
     email_id = data[0].split()[0]
-    status, data = M.fetch(email_id, '(RFC822)')
+    status, data = M.fetch(email_id, "(RFC822)")
     msg = email.message_from_string(data[0][1])
-    code = re.search('<code>(.*)</code>', msg.get_payload()).group(1)
+    code = re.search("<code>(.*)</code>", msg.get_payload()).group(1)
     if code == messageID:
-        if (config.getboolean('Mail', 'delete_test_email')):
-            M.store(email_id, '+FLAGS', '\\Deleted')
+        if config.getboolean("Mail", "delete_test_email"):
+            M.store(email_id, "+FLAGS", "\\Deleted")
             M.expunge()
             M.close()
             M.logout()
@@ -201,7 +200,7 @@ def checkMail():
     return False, "Email was not correctly received"
 
 config = ConfigParser.ConfigParser()
-config.read('trimon.conf')
+config.read("trimon.conf")
 
 checks = ["Ping", "Ports", "Load", "Mem", "Mail", "Webs"]
 
@@ -209,18 +208,18 @@ all_success = True
 signal.signal(signal.SIGALRM, handler)
 
 for check in checks:
-    if config.getboolean(check, 'enabled'):
+    if config.getboolean(check, "enabled"):
         signal.alarm(20)
         try:
-            methodToCall = locals()['check' + check]
+            methodToCall = locals()["check" + check]
             success, reason = methodToCall()
             if not success:
                 all_success = False
                 sendAlert(check, reason)
         except Exception as e:
             all_success = False
-            sendAlert(check, str(e), 'exception')
+            sendAlert(check, str(e), "exception")
         signal.alarm(0)
 
-if config.getboolean('General', 'mail_success') and all_success:
-    sendAlert('any', 'all tests passed', 'success')
+if config.getboolean("General", "mail_success") and all_success:
+    sendAlert("any", "all tests passed", "success")
